110d0d43553a8d1707fad6e1c902cf9173d24fa4
hugh-bowie <hughbowie@me.com>
2021-08-03 15:28:58 -0400

    2.5 code snapshot pushed

--- client/src/App.js
+++ client/src/App.js
@@ -14,31 +14,40 @@ import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
 const client = new ApolloClient({
-	uri: '/graphql',
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
 });
 
 function App() {
-	return (
-		<ApolloProvider client={client}>
-			<Router>
-				<div className='flex-column justify-flex-start min-100-vh'>
-					<Header />
-					<div className='container'>
-						<Switch>
-							<Route exact path='/' component={Home} />
-							<Route exact path='/login' component={Login} />
-							<Route exact path='/signup' component={Signup} />
-							<Route exact path='/profile/:username?' component={Profile} />
-							<Route exact path='/thought/:id' component={SingleThought} />
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
 
-							<Route component={NoMatch} />
-						</Switch>
-					</div>
-					<Footer />
-				</div>
-			</Router>
-		</ApolloProvider>
-	);
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
